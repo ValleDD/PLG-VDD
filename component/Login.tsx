f11c7e04a551b129bcc2a5779d1267cad323c394
valleDD <mvalledeniz@gmail.com>
2023-12-04 19:55:02 +0000

    retoque de login y configuracion booton welcom

--- component/Login.tsx
+++ component/Login.tsx
@@ -1,44 +1,73 @@
-import React from 'react'
-import { StyleSheet, View, Text, Image,Button, SafeAreaView, TextInput } from "react-native";
-import appColors from '../assets/style/appColors';
-
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  Button,
+  SafeAreaView,
+  TextInput,
+  Alert,
+} from "react-native";
+import appColors from "../assets/style/appColors";
 
 const Login = () => {
-  const [text, onChangeText] = React.useState('Useless Text');
-  const [number, onChangeNumber] = React.useState('');
+  const [usuario, setUsuario] = useState("");
+  const [contraseña, setContraseña] = useState("");
+
+  const handleLogin = () => {
+    if (usuario === "usuario" && contraseña === "contraseña") {
+      Alert.alert("Login exitoso", "Bienvenido");
+    } else {
+      Alert.alert(
+        "Error de inicio de sesión",
+        "Usuario o contraseña incorrectos"
+      );
+    }
+  };
   return (
-    
-      <View style={styles.fondo}>
-     <SafeAreaView>
-    
-    <TextInput
-      style={styles.input}
-      onChangeText={onChangeNumber}
-      placeholder="Nombre"
-    />
-  </SafeAreaView>
+    <View style={styles.fondo}>
+      <SafeAreaView style={styles.container}>
+        <Text>Usuario: </Text>
+        <TextInput
+          placeholder="Usuario"
+          style={styles.input}
+          value={usuario}
+          onChangeText={(text) => setUsuario(text)}
+        />
+        <Text>Contraseña</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Contraseña"
+          secureTextEntry
+          value={contraseña}
+          onChangeText={(text) => setContraseña(text)}
+        />
+        <Button title="Iniciar Sesión" onPress={handleLogin} />
+      </SafeAreaView>
+    </View>
+  );
+};
 
-      </View>
-     
-    
-    
-  
-  )
-}
-
-const styles= StyleSheet.create({
-fondo:{
-  backgroundColor: appColors.primary,
-  height: '100%',
-  width: 'auto'
-  
-},
-input: {
-  height: 40,
-  margin: 12,
-  borderWidth: 1,
-  padding: 10,
-},
-})
-export default Login
-
+const styles = StyleSheet.create({
+  fondo: {
+    backgroundColor: appColors.primary,
+    height: "100%",
+    width: "auto",
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 16,
+  },
+  input: {
+    height: 40,
+    width: "80%",
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 16,
+    paddingLeft: 8,
+  },
+});
+export default Login;
--- component/Welcom.tsx
+++ component/Welcom.tsx
@@ -1,75 +1,68 @@
 import React, { useState } from "react";
-import { StyleSheet, View, Text, Image,Button, Modal, Alert, Pressable } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  Button,
+  Modal,
+  Alert,
+  Pressable,
+} from "react-native";
 import appColors from "../assets/style/appColors";
 import Login from "./Login";
+import { useNavigation } from "@react-navigation/native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
-const Welcom = () => {
-  
-  const [modalVisible, setModalVisible] = useState(false);
+const Welcom = ({ navigation }) => {
   return (
-    <View  style={styles.fondo}>
+    <View style={styles.fondo}>
       <View>
         <Image
           style={styles.imagen}
           source={require("./../assets/Logo-PLG.png")}
         />
-      </View >
-      <View >
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => {
-          Alert.alert('Modal has been closed.');
-          setModalVisible(!modalVisible);
-        }}>
-        <View >
-          <View >
-            <Login/>
-          </View>
-        </View>
-      </Modal>
-      <Pressable
+      </View>
+      <View>
+      <Text>Bienvenidos</Text>
+      </View>
+      <View>
+        <Button title="boton" onPress={() => navigation.navigate("Login")}/>
       
-        onPress={() => setModalVisible(true)}>
-        <Text>Show Modal</Text>
-      </Pressable>
-    </View>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   imagen: {
-    alignItems:"center",
-    alignContent:"center",
+    alignItems: "center",
+    alignContent: "center",
     height: 200,
     width: 200,
   },
-  
-  text:{
-    
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding:50,
-    
+
+  text: {
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 50,
   },
-  colorText:{
-    color: 'white',
-    fontSize:24,
+  colorText: {
+    color: "white",
+    fontSize: 24,
   },
-  Bstyle:{
-    alignItems: 'center',
-    width:100,
+  Bstyle: {
+    alignItems: "center",
+    width: 100,
   },
-  fondo:{
+  fondo: {
     backgroundColor: appColors.primary,
-    width: '100%',
-    height: '100%',
-    alignContent: 'center',
-    alignItems: 'center',
+    width: "100%",
+    height: "100%",
+    alignContent: "center",
+    alignItems: "center",
   },
- 
+  boton: {},
 });
 
 export default Welcom;
