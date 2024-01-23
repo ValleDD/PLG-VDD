cc3ced3c2806368a8fc8d8305486a6c9f876ceb7
valleDD <mvalledeniz@gmail.com>
2024-01-23 16:25:37 +0000

    feat: poner el newUser al context y cuando se registre pasar al login

--- Screems/Register.tsx
+++ Screems/Register.tsx
@@ -1,63 +1,90 @@
 import React, { useContext, useState } from "react";
-import { View, Text, TextInput, StyleSheet, Button, Pressable } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  Button,
+  Pressable,
+} from "react-native";
 import appColors from "../assets/style/appColors";
 import { User, UserContext } from "../context/UserContext";
 
-const Register:React.FC = () => {
-    const [name,setName]= useState<String>('');
-    const [password,setPassword]= useState<String>('');
-    const [email,setEmail]= useState<String>('');
+interface RegisterProps{
+  navigation: any;
+}
 
-    const handleRegister=async () => {
-        try {
-            const response = await fetch('http://172.16.100.165:8888/users/register', {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify({
-                name: name,
-                email: email,
-                password: password
-                
-              }),
-            });
-            if (!response.ok) {
-                throw new Error("error en la solicitud");
-                
-            }
-            
+const Register: React.FC<RegisterProps> = ({navigation}) => {
+  const [name, setName] = useState<String>("");
+  const [password, setPassword] = useState<String>("");
+  const [email, setEmail] = useState<String>("");
+  
+
+  const { setUser } = useContext(UserContext);
+
+  const handleRegister = async () => {
+    try {
+      const response = await fetch(
+        "http://192.168.1.36:8888/users/register",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name,
+            email: email,
+            password: password,
+          }),
+        }
+      );
+      if (!response.ok) {
+        throw new Error("error en la solicitud," + response.status);
+      }
+
       // Si la solicitud es exitosa, actualiza el contexto con el nuevo usuario registrado
       const newUser: User = await response.json();
-      setName('');
-      console.log('Registro exitoso:', newUser);
-     
+      setUser(newUser);
+      setName("");
+      console.log("Registro exitoso:", newUser);
+      navigation.navigate('Login')
      
     } catch (error) {
-      console.error('Error al registrar el usuario', error);
+      console.error("Error al registrar el usuario", error);
     }
-  
-        
-    }
-    
+  };
 
   return (
     <View style={styles.fondo}>
-      <View style={styles.centrarTt} >
+      <View style={styles.centrarTt}>
         <Text style={styles.titulo}>Nuevo Registro</Text>
       </View>
 
       <View style={styles.container}>
         <Text>Nombre:</Text>
-        <TextInput   placeholder="Ingresa tu nombre" onChangeText={(text)=>setName(text)}  style={styles.input} />
+        <TextInput
+          placeholder="Ingresa tu nombre"
+          onChangeText={(text) => setName(text)}
+          style={styles.input}
+        />
         <Text>Email:</Text>
-        <TextInput placeholder="Ingrese su email" onChangeText={(text)=>setEmail(text)} keyboardType="email-address" style={styles.input} />
+        <TextInput
+          placeholder="Ingrese su email"
+          onChangeText={(text) => setEmail(text)}
+          keyboardType="email-address"
+          style={styles.input}
+        />
         <Text>Contraseña:</Text>
-        <TextInput  onChangeText={(text)=> setPassword(text)} placeholder="Ingrese su contraseña" secureTextEntry style={styles.input} />
+        <TextInput
+          onChangeText={(text) => setPassword(text)}
+          placeholder="Ingrese su contraseña"
+          secureTextEntry
+          style={styles.input}
+        />
       </View>
       <View style={styles.centrarButton}>
         <Pressable style={styles.stButtons} onPress={handleRegister}>
-            <Text style={styles.textButton}>Register</Text>
+          <Text style={styles.textButton}>Register</Text>
         </Pressable>
       </View>
     </View>
@@ -80,41 +107,38 @@ const styles = StyleSheet.create({
     borderColor: "black",
     borderWidth: 2,
     marginBottom: 18,
-    paddingLeft: 'auto',
-    borderRadius:23,
-    paddingTop:'auto',
-   
-   
+    paddingLeft:52,
+    borderRadius: 23,
+    paddingTop: "auto",
   },
-  titulo:{
-    fontWeight: 'bold',
+  titulo: {
+    fontWeight: "bold",
     fontSize: 23,
   },
-  centrarTt:{
-    alignItems: 'center',
-    paddingTop:40,
-    paddingBottom:80
+  centrarTt: {
+    alignItems: "center",
+    paddingTop: 40,
+    paddingBottom: 80,
   },
-  stButtons:{
-    width:'50%',
-    height:'20%',
-    lineHeight: 'auto',
-    alignItems: 'center',
-    borderRadius:34,
-    borderColor: 'black',
-    backgroundColor: 'white',
+  stButtons: {
+    width: "50%",
+    height: "20%",
+    lineHeight: "auto",
+    alignItems: "center",
+    borderRadius: 34,
+    borderColor: "black",
+    backgroundColor: "white",
   },
   textButton: {
     fontSize: 23,
     lineHeight: 30,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     letterSpacing: 0.25,
-    
   },
-  centrarButton:{
-    alignContent: 'center',
-    alignItems: 'center'
-  }
+  centrarButton: {
+    alignContent: "center",
+    alignItems: "center",
+  },
 });
 
 export default Register;
